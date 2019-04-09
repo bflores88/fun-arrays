f7f566fa55ecbecbdd14d903edc25a4e764458d0
kmietotwy <kmietotwy@gmail.com>
2019-04-08 14:16:59 -1000

    stretch goal?

--- array-methods.js
+++ array-methods.js
@@ -5,23 +5,35 @@ var dataset = require('./dataset.json');
   greater than 100000
   assign the resulting new array to `hundredThousandairs`
 */
-var hundredThousandairs = null;
+var hundredThousandairs = dataset.bankBalances.filter(function (element) {
+  return element.amount > 100000;
+});
 
 // set sumOfBankBalances to be the sum of all value held at `amount` for each bank object
-var sumOfBankBalances = null;
+var sumOfBankBalances = dataset.bankBalances.map(function (element) {
+  return element.amount
+}).reduce(function (previous, current) {
+  return parseInt(previous) + parseInt(current);
+})
 
 /*
   from each of the following states:
-    Wisconsin
-    Illinois
-    Wyoming
-    Ohio
-    Georgia
-    Delaware
+    Wisconsin = WI
+    Illinois = IL
+    Wyoming = WY
+    Ohio = OH
+    Georgia = GA
+    Delaware - DE
   take each `amount` and add 18.9% interest to it rounded to the nearest dollar 
   and then sum it all up into one value saved to `sumOfInterests`
  */
-var sumOfInterests = null;
+var sumOfInterests = dataset.bankBalances.filter(function (element) {
+  return element.state === 'WI' || element.state === 'IL' || element.state === 'WY' || element.state === 'OH' || element.state === 'GA' || element.state === 'DE';
+}).map(function (element) {
+  return parseInt(element.amount) * 0.189;
+}).reduce(function (previous, current) {
+  return Math.round(previous + current);
+})
 
 /*
   aggregate the sum of bankBalance amounts
@@ -39,39 +51,73 @@ var sumOfInterests = null;
     round this number to the nearest dollar before moving on.
   )
  */
-var stateSums = null;
+var stateSums = {};
 
-/*
-  for all states *NOT* in the following states:
-    Wisconsin
-    Illinois
-    Wyoming
-    Ohio
-    Georgia
-    Delaware
-  sum the amount for each state (stateSum)
-  take each `stateSum` and calculate 18.9% interest for that state
-  sum the interest values that are greater than 50,000 and save it to `sumOfHighInterests`
+dataset.bankBalances.forEach(function(elem){
+  if(!stateSums.hasOwnProperty(elem.state)){
+    stateSums[elem.state] = parseInt(elem.amount);
+  } else {
+    stateSums[elem.state] += parseInt(elem.amount)
+  }
+})
 
-  note: During your summation (
-    if at any point durig your calculation where the number looks like `2486552.9779399997`
-    round this number to the nearest dollar before moving on.
-  )
- */
-var sumOfHighInterests = null;
+  /*
+    for all states *NOT* in the following states:
+      Wisconsin
+      Illinois
+      Wyoming
+      Ohio
+      Georgia
+      Delaware
+    sum the amount for each state (stateSum)
+    take each `stateSum` and calculate 18.9% interest for that state
+    sum the interest values that are greater than 50,000 and save it to `sumOfHighInterests`
+
+    note: During your summation (
+      if at any point durig your calculation where the number looks like `2486552.9779399997`
+      round this number to the nearest dollar before moving on.
+    )
+   */
+  var sumOfHighInterests = Object.keys(stateSums).map(function(elem){
+    return [elem, stateSums[elem]];
+  }).filter(function(elem){
+    return elem[0] !== 'WI' && elem[0] !== 'IL' && elem[0] !== 'WY' && elem[0] !== 'OH' && elem[0] !== 'GA' && elem[0] !== 'DE';
+  }).map(function(elem){
+    return Math.round(elem[1] * 0.189);
+  }).filter(function(elem){
+    return elem > 50000;
+  }).reduce(function(previous, current){
+    return previous + current;
+  });
 
 /*
   set `lowerSumStates` to be an array of two letter state
   abbreviations of each state where the sum of amounts
   in the state is less than 1,000,000
  */
-var lowerSumStates = null;
+var lowerSumStates = Object.keys(stateSums).map(function(elem){
+  return [elem, stateSums[elem]];
+}).filter(function(elem){
+  return elem[1] < 1000000;
+}).map(function(elem){
+  return elem[0];
+})
 
 /*
   aggregate the sum of each state into one hash table
   `higherStateSums` should be the sum of all states with totals greater than 1,000,000
  */
-var higherStateSums = null;
+var higherStateSums = Object.keys(stateSums).map(function(elem){
+  return [elem, stateSums[elem]]
+}).filter(function(elem){
+  return elem[1] > 1000000;
+}).map(function(elem){
+  return elem[1];
+}).reduce(function(previous, current){
+  return previous + current;
+})
+
+console.log(higherStateSums);
 
 /*
   from each of the following states:
@@ -88,7 +134,13 @@ var higherStateSums = null;
   if true set `areStatesInHigherStateSum` to `true`
   otherwise set it to `false`
  */
-var areStatesInHigherStateSum = null;
+var areStatesInHigherStateSum = Object.keys(stateSums).map(function(elem){
+  return [elem, stateSums[elem]]
+}).filter(function(elem){
+  return elem[0] === 'WI' || elem[0] === 'IL' || elem[0] === 'WY' || elem[0] === 'OH' || elem[0] === 'GA' || elem[0] === 'DE';
+}).every(function(elem){
+  return elem[1] > 2550000;
+})
 
 /*
   Stretch Goal && Final Boss
@@ -104,17 +156,22 @@ var areStatesInHigherStateSum = null;
   have a sum of account values greater than 2,550,000
   otherwise set it to be `false`
  */
-var anyStatesInHigherStateSum = null;
-
+var anyStatesInHigherStateSum = Object.keys(stateSums).map(function(elem){
+  return [elem, stateSums[elem]]
+}).filter(function(elem){
+  return elem[0] === 'WI' || elem[0] === 'IL' || elem[0] === 'WY' || elem[0] === 'OH' || elem[0] === 'GA' || elem[0] === 'DE';
+}).some(function(elem){
+  return elem[1] > 2550000;
+});
 
 module.exports = {
-  hundredThousandairs : hundredThousandairs,
-  sumOfBankBalances : sumOfBankBalances,
-  sumOfInterests : sumOfInterests,
-  sumOfHighInterests : sumOfHighInterests,
-  stateSums : stateSums,
-  lowerSumStates : lowerSumStates,
-  higherStateSums : higherStateSums,
-  areStatesInHigherStateSum : areStatesInHigherStateSum,
-  anyStatesInHigherStateSum : anyStatesInHigherStateSum
+  hundredThousandairs: hundredThousandairs,
+  sumOfBankBalances: sumOfBankBalances,
+  sumOfInterests: sumOfInterests,
+  sumOfHighInterests: sumOfHighInterests,
+  stateSums: stateSums,
+  lowerSumStates: lowerSumStates,
+  higherStateSums: higherStateSums,
+  areStatesInHigherStateSum: areStatesInHigherStateSum,
+  anyStatesInHigherStateSum: anyStatesInHigherStateSum
 };
